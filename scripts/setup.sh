b66bcf83dd02a65b465751606df0694cfdfbbad4
Leymora <13785032+Leymora@users.noreply.github.com>
2025-05-30 22:35:18 +0200

    Added festive modes :)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -15,6 +15,7 @@ modules_left=$(get_option "@celia-modules-left" "git")
 amount_modules_left_pre=$(echo "$modules_left" | grep -o ' ' | wc -l)
 amount_modules_left=$(($amount_modules_left_pre + 1))
 disp_repo_name=$(get_option "@celia-repo-name" "hide")
+festive=$(get_option "@celia-festive" true)
 
 # Separators
 ll_sep=$(get_option "@celia-ll-sep" "")
@@ -29,7 +30,7 @@ icon_offline=$(get_option "@celia-icon-offline" "󰌙")
 icon_clock=$(get_option "@celia-icon-clock" "󰥔")
 icon_git=$(get_option "@celia-icon-git" "")
 icon_repo_none=$(get_option "@celia-icon-repo-none" "")
-icon_repo=$(get_option "@celia-icon-repo-none" "")
+icon_repo=$(get_option "@celia-icon-repo" "")
 icon_branch=$(get_option "@celia-icon-branch" "󰘬")
 icon_added=$(get_option "@celia-icon-added" "")
 icon_modified=$(get_option "@celia-icon-modified" "")
@@ -41,3 +42,18 @@ col_battery=$(get_option "@celia-color-battery" "$col_blue")
 col_network=$(get_option "@celia-color-network" "$col_yellow")
 col_clock=$(get_option "@celia-color-clock" "$col_green")
 col_git=$(get_option "@celia-color-git" "$col_green")
+
+# Festivities!
+if [ $festive == true ]; then
+  if [[ $(date +"%m") == 05 ]]; then # Check if it's October
+   icon_battery="󰮣"
+   icon_wifi=""
+   icon_clock=""
+   icon_repo_none=""
+   icon_repo="󰗚"
+   col_battery=$col_yellow
+   col_network=$col_yellow
+   col_clock=$col_yellow
+   col_git=$col_yellow
+ fi
+fi

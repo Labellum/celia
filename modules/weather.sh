84e4b4a4583ccd622b465bacf7f9a34060ba2e87
Leymora <13785032+Leymora@users.noreply.github.com>
2025-06-01 23:27:26 +0200

    Fixed padding issue in Weather module

--- modules/weather.sh
+++ modules/weather.sh
@@ -5,4 +5,4 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$CURRENT_DIR/../scripts/functions.sh"
 source "$CURRENT_DIR/../scripts/setup.sh"
 
-tmux set-option -ga status-right "#[fg=$col_weather]$rl_sep#[bg=$col_weather]#[fg=$col_black]#(source '$CURRENT_DIR/../scripts/checkWeatherIcon.sh')#(echo ' ')#[fg=$col_weather]#[bg=$col_black]#(echo ' ')#(source '$CURRENT_DIR/../scripts/getWeather.sh')#[fg=$col_black]#[bg=$col_back]$rr_sep#(echo ' ')"
+tmux set-option -ga status-right "#[fg=$col_weather]$rl_sep#[bg=$col_weather]#[fg=$col_black]#(source '$CURRENT_DIR/../scripts/checkWeatherIcon.sh')#(echo ' ')#[fg=$col_weather]#[bg=$col_black]#(echo ' ')#(source '$CURRENT_DIR/../scripts/getWeather.sh')#(echo ' ')#[fg=$col_black]#[bg=$col_back]$rr_sep#(echo ' ')"

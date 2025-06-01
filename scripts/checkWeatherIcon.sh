4a3ed07615d17370f582e1e7e1f05c9ce129b06a
Leymora <13785032+Leymora@users.noreply.github.com>
2025-06-01 23:25:12 +0200

    This weather module is confusing me so much

--- scripts/checkWeatherIcon.sh
+++ scripts/checkWeatherIcon.sh
@@ -5,7 +5,7 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$CURRENT_DIR/setup.sh"
 
 city=$(curl -s ifconfig.co/city)
-weather_cond=$(curl -s wttr.in/$city?format=%C&nonce=$RANDOM)
+weather_cond=$(curl -s "wttr.in/$city?format=%C&nonce=$RANDOM")
 
     case $weather_cond in
     "Partly cloudy")

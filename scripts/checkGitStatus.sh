ee89c7f829fcf587104d685ab5dcf4c4dacbcddf
Leymora <13785032+Leymora@users.noreply.github.com>
2025-07-13 20:15:25 +0200

    Fixed the git module that somehow broke?? Maybe tmux updated something

--- scripts/checkGitStatus.sh
+++ scripts/checkGitStatus.sh
@@ -3,7 +3,7 @@
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$CURRENT_DIR/setup.sh"
 
-git_dir="$(tmux display-message -p -F "#{pane_current_path}" -t0)/.git"
+git_dir="$(tmux display-message -p -F "#{pane_current_path}")/.git"
 repo_name="$(basename "$(git -C "$git_dir/.." config --get remote.origin.url)" | awk -F'.' '{print $1}')"
 
 # Check if there is a ".git" directory in the current directory
